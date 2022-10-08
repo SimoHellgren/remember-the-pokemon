a9ee42d7c8a17eb16d973f05c2d20b2ec277ec02
Simo Hellgrén <simo.hellgren@relex.fi>
2022-10-08 13:30:30 +0300

    fix a few names for humans

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,15 @@
 export async function load({ fetch }) {
   let response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
   let data = await response.json()
+
+  let pokemon = await data.results
+
+  // preprocess certain names
+  pokemon = pokemon
+    .map(p => p.name.includes("nidoran") ? { ...p, name: "nidoran" } : p)
+    .map(p => p.name === "mr-mime" ? { ...p, name: "mr mime" } : p)
+
   return {
-    pokemon: await data.results
+    pokemon
   }
 }
