eaf4182558be1fc7bc5a6fd8872e7707e03ab210
Simo Hellgrén <simo.hellgren@relex.fi>
2022-10-10 19:51:21 +0300

    typo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	build: {
-		outdir: '/var/www/pokemon',
+		outDir: '/var/www/pokemon',
 		emptyOutDir: true,
 	}
 };
