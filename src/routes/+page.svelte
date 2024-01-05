ae20466dadf71ff13dc56e331e0a55e339562c07
SimoHellgren <hellgrensimo@gmail.com>
2024-01-05 20:24:56 +0200

    mvp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Pokemon from './Pokemon.svelte';
+	export let data;
+	let hints = false;
+
+	let pokemon = data.pokemon.map((row, i) => ({
+		id: i + 1,
+		name: row.name,
+		hint: false,
+		solved: false
+	}));
+
+	let guess = '';
+	$: {
+		let match = pokemon.find((p) => p.name === guess.toLowerCase());
+		if (match) {
+			pokemon = pokemon.map((p) => (p.id === match.id ? { ...match, solved: true } : p));
+			guess = '';
+		}
+	}
+
+	const toggleHints = () => {
+		hints = !hints;
+		pokemon = pokemon.map((p) => ({ ...p, hint: hints }));
+	};
+</script>
+
+<input bind:value={guess} placeholder="type here" />
+<button on:click={toggleHints}>{hints ? 'Hints on' : 'Hints off'}</button>
+<div class="container">
+	{#each pokemon as pokeman}
+		<Pokemon
+			id={pokeman.id}
+			name={pokeman.name}
+			bind:hint={pokeman.hint}
+			bind:solved={pokeman.solved}
+			on:click
+		/>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
---
+++ src/routes/Pokemon.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let id;
+	export let name;
+	export let hint;
+	export let solved;
+
+	const urlTemplate = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+	let placeholder = urlTemplate + '0.png';
+	let image = urlTemplate + id + '.png';
+</script>
+
+<div on:click={() => (hint = !hint)} class="container">
+	{#if !solved}
+		{#if hint}
+			<img src={image} class="silhouette" alt="Who's that Pokémon?" />
+		{:else}
+			<img src={placeholder} alt="Who's that Pokémon?" />
+		{/if}
+	{:else}
+		<img src={image} alt={name} />
+		<p>#{id} {name.toUpperCase()}</p>
+	{/if}
+</div>
+
+<style>
+	.container {
+		text-align: center;
+	}
+	.silhouette {
+		filter: grayscale(1) contrast(0);
+	}
+</style>
