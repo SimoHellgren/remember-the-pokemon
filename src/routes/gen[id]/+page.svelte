581a25f97acdfb3759be7841c099974447556c2f
SimoHellgren <hellgrensimo@gmail.com>
2024-01-08 17:16:40 +0200

    nicer menu

--- src/routes/gen[id]/+page.svelte
+++ src/routes/gen[id]/+page.svelte
@@ -37,6 +37,19 @@
 </script>
 
 <div class="controls">
+	<nav>
+		<span>
+			<span>Menu</span>
+			<img src="/favicon.png" alt="Menu" />
+		</span>
+		<ul>
+			{#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as gen}
+				<li>
+					<a href={`/gen${gen}`} data-sveltekit-reload>Gen {gen}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
 	<input autofocus bind:value={guess} placeholder="type here" />
 	<label>
 		Show all hints:
@@ -47,14 +60,6 @@
 		<progress value={guessed} max={total} />
 	</label>
 	<span>Time elapsed: {minutes}:{seconds}</span>
-	<span>
-		Choose gen:
-		<nav>
-			{#each [1, 2, 3, 4, 5, 6, 7, 8, 9] as gen}
-				<a href={`/gen${gen}`} data-sveltekit-reload>{gen}</a>
-			{/each}
-		</nav>
-	</span>
 </div>
 
 <div class="container">
@@ -70,6 +75,73 @@
 </div>
 
 <style>
+	:root {
+		--animation-duration: 300ms;
+	}
+
+	nav {
+		display: flex;
+		flex-direction: column;
+		align-self: flex-start;
+		align-items: center;
+		justify-content: center;
+		width: 2rem;
+		height: 2rem;
+		transition: width var(--animation-duration) ease,
+			background-color var(--animation-duration) ease, border-style var(--animation-duration) ease;
+	}
+
+	nav span {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	nav span > span {
+		display: none;
+	}
+
+	nav img {
+		display: inline-block;
+		width: 2rem;
+		height: 2rem;
+		transform: rotate(0deg);
+		transition: transform var(--animation-duration) ease;
+	}
+
+	nav:hover {
+		background-color: #fefefe;
+		border-style: double;
+		width: 5rem;
+		height: fit-content;
+	}
+
+	nav:hover img {
+		transform: rotate(360deg);
+	}
+
+	nav:hover span > span {
+		display: inline-block;
+	}
+
+	nav ul {
+		display: none;
+		list-style: none;
+		padding: 0;
+		gap: 0.5rem;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	nav:hover ul {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
 	.controls {
 		position: fixed;
 		left: 0;
@@ -79,27 +151,15 @@
 		height: 2rem;
 		width: 100%;
 		background-color: #ffffff;
-		display: inline;
+		display: flex;
+		align-items: center;
+		column-gap: 0.3rem;
 	}
+
 	.container {
 		margin-top: 3rem;
 		display: flex;
 		flex-wrap: wrap;
 		gap: 1rem;
-	}
-
-	nav {
-		display: inline;
-	}
-
-	a {
-		background-color: #ebb5e6;
-		border: 1px solid #b885b4;
-		border-radius: 3px;
-		padding: 0.2rem 0.5rem 0.2rem 0.5rem;
-		margin: 0.1rem;
-		text-align: center;
-		width: fit-content;
-		text-decoration: none;
 	}
 </style>
